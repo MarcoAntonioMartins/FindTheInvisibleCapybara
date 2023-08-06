9cfb9a063d51a64d56983c1b777648bfeecc4083
Marco Martins <marco.martins22@hotmail.com>
2023-08-06 16:59:54 -0300

    Feat: add description of how to play

--- src/screens/index.js
+++ src/screens/index.js
@@ -8,6 +8,7 @@ function Index({ startGame }) {
             <img src={logo_capybara} className="logo_capybara" alt="Random Capybara" />
             <h1>Find the Invisible Capybara</h1>
             <button className="ready_link" onClick={startGame}>Start</button>
+            <p className="details_info">Move the mouse around the screen to find the Capybara. The closer you get, the louder the music will be.</p>
         </header>
     </div>
   );
